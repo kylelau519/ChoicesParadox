a7b89c45797b9653db46030f08a68f3c1871a790
Harry Y. <harryyu02@gmail.com>
2026-04-07 22:06:22 -0400

    feat: implement basic scraper

--- run_scrapper/README.md
+++ run_scrapper/README.md
@@ -1,1 +1,8 @@
-#Run Scrapper
+# Run Scrapper
+This directory contains the scraper for Choices Paradox.
+
+## Usage
+Runs will be scraped to `./runs/`
+```bash
+uv run main.py
+```
---
+++ run_scrapper/scraper.py
@@ -0,0 +1,36 @@
+import requests
+
+class STS2RunsScraper:
+    runs_url = "https://sts2runs.com/api/runs/community"
+    run_base_url = "https://sts2runs.com/api/runs"
+    data = []
+    pagination = {
+        "page": 1,
+        "limit": 25,
+        "total": 0,
+        "totalPages": 1,
+    }
+
+    def __init__(self):
+        return
+
+    def get_run(self, id):
+        url = self.run_base_url + "/" + str(id)
+        print(url)
+        r = requests.get(url)
+        resp = r.json()
+        return resp
+
+    def scrape(self):
+        while self.pagination["page"] <= self.pagination["totalPages"]:
+            r = requests.get(self.runs_url, params={"page": self.pagination["page"]})
+            resp = r.json()
+
+            for run in resp["runs"]:
+                run = self.get_run(run["id"])
+                self.data.append(run)
+
+            self.pagination = resp["pagination"]
+            self.pagination["page"] += 1
+
+

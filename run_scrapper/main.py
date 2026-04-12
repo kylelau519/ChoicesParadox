3063611b8cc0cdafa8e7418162b88bc7dad39dbb
Harry Y. <harryyu02@gmail.com>
2026-04-11 22:45:25 -0400

    feat: add spire-codex scraper

--- run_scrapper/main.py
+++ run_scrapper/main.py
@@ -1,13 +1,14 @@
 import json
 import os
 
-from .scraper import STS2RunsScraper
+from scraper import STS2RunsScraper, SpireCodexScraper
 
 
 def main():
+    codex = SpireCodexScraper()
+    codex.scrape()
     s = STS2RunsScraper()
     s.scrape()
-    print(f"Succssfully scraped {len(s.data)} runs.")
 
     for r in s.data:
         run = r["run"]
@@ -33,7 +34,32 @@ def main():
             print(f"Saving {path}")
             json.dump(run, f, indent=4)
 
-    print(f"Succssfully scraped {len(s.data)} runs.")
+    for run in codex.data:
+        # skip multi-player games
+        players = run.get("players")
+        if players == None:
+            continue
+        if len(players) != 1:
+            continue
+
+        player = players[0]
+        ascension = f"a{str(run['ascension'])}"  # 0 -> a0
+        character = (
+            player["character"].split(".")[-1].lower()
+        )  # "CHARACTER.REGENT" -> "regent"
+        build_id = run["build_id"]
+        dir = f"data/runs/{build_id}/{character}/{ascension}"
+        os.makedirs(dir, exist_ok=True)
+
+        seed = run["seed"]
+        start_time = run["start_time"]
+        path = f"{dir}/{seed}_{start_time}.run"
+        with open(path, "w") as f:
+            print(f"Saving {path}")
+            json.dump(run, f, indent=4)
+
+    print(f"Succssfully scraped {len(codex.data)} runs from spire-codex.")
+    print(f"Succssfully scraped {len(s.data)} runs from sts2runs.")
 
 
 if __name__ == "__main__":
--- run_scrapper/scraper.py
+++ run_scrapper/scraper.py
@@ -1,19 +1,64 @@
+from typing import Any
 import requests
 
 
-class STS2RunsScraper:
-    runs_url = "https://sts2runs.com/api/runs/community"
-    run_base_url = "https://sts2runs.com/api/runs"
-    data = []
-    pagination = {
-        "page": 1,
-        "limit": 25,
-        "total": 0,
-        "totalPages": 1,
-    }
+class BaseScraper:
+    def __init__(self, runs_url: str, run_base_url: str, pagination: dict[str, int]):
+        self.runs_url: str = runs_url
+        self.run_base_url: str = run_base_url
+        self.data: list[Any] = []
+        self.pagination: dict[str, int] = pagination
 
+    def scrape(self):
+        raise NotImplemented("Not implemented")
+
+class SpireCodexScraper(BaseScraper):
     def __init__(self):
-        return
+        super().__init__(
+            "https://spire-codex.com/api/runs/list",
+            "https://spire-codex.com/api/runs/shared",
+            {
+                "total": 0,
+                "page": 1,
+                "per_page": 50,
+                "total_pages": 1,
+            }
+        )
+
+    def get_run(self, id: str):
+        url = self.run_base_url + "/" + id
+        print(url)
+        r = requests.get(url)
+        resp = r.json()
+        return resp
+
+    def scrape(self):
+        while self.pagination["page"] <= self.pagination["total_pages"]:
+            r = requests.get(self.runs_url, params={"page": self.pagination["page"]})
+            resp = r.json()
+
+            for run in resp["runs"]:
+                run = self.get_run(run["run_hash"])
+                self.data.append(run)
+
+            self.pagination["total"] = resp["total"]
+            self.pagination["page"] = resp["page"]
+            self.pagination["per_page"] = resp["per_page"]
+            self.pagination["total_pages"] = resp["total_pages"]
+            self.pagination["page"] += 1
+
+class STS2RunsScraper(BaseScraper):
+    def __init__(self):
+        super().__init__(
+            "https://sts2runs.com/api/runs/community",
+            "https://sts2runs.com/api/runs",
+            {
+                "page": 1,
+                "limit": 25,
+                "total": 0,
+                "totalPages": 1,
+            }
+        )
 
     def get_run(self, id):
         url = self.run_base_url + "/" + str(id)

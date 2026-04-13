bf911ec8d52948c1cd032976c6b700bf74cf409f
Ho Chun Lau <39511811+kylelau519@users.noreply.github.com>
2026-04-12 19:51:17 -0700

    Added test case and make sure adding card will change the result

--- stat_analysis/tests/test_eval.py
+++ stat_analysis/tests/test_eval.py
@@ -1,7 +1,11 @@
 import unittest
 
 import joblib
+
+from run_preprocessor.save_reader import CurrentSaveReader
+from run_preprocessor.snapshot import PlayerSnapshot
 from stat_analysis.eval import Evaluator
+from stat_analysis.state_vectorizer import TestCaseGenerator
 
 
 class TestEval(unittest.TestCase):
@@ -16,9 +20,52 @@ class TestEval(unittest.TestCase):
             show_event_cards=False,
             show_encounters=False,
         )
-        print("Top 50 Important Features:")
-        for feature, importance in features:
-            print(f"{feature}: {importance:.4f}")
+
+    def test_card_choices_changes(self):
+        eval = Evaluator("testfiles/xgb_model.joblib")
+        run = CurrentSaveReader.from_file("testfiles/current_run.save")
+        snapshot = PlayerSnapshot(run)
+        generator = TestCaseGenerator(snapshot)
+        generator.set_max_health(28)
+        print(snapshot.deck.cards)
+        generator.set_encounter("ENCOUNTER.SKULKING_COLONY_ELITE")
+        x_original = generator.vectorize()
+        pred_original = eval.predict(x_original)
+        # print(
+        #     f"Original prediction against skulking colony elite: {pred_original[0]:.2f}"
+        # )
+
+        card_choices = ["CARD.TWIN_STRIKE", "CARD.FLAME_BARRIER"]
+        test_cases, labels = generator.test_adding_cards(card_choices)
+        for case, label in zip(test_cases, labels):
+            pred = eval.predict(case)
+            if label == "Original":
+                self.assertEqual(
+                    pred, pred_original, "Original case should have the same prediction"
+                )
+            else:
+                self.assertNotEqual(
+                    pred,
+                    pred_original,
+                    f"Prediction should change after taking {label}",
+                )
+        # for case, label in zip(test_cases, labels):
+        #     print(f"Test case for taking {label}: Non-zero features:")
+        #     non_zero_idx = case.nonzero()[1]
+        #     features_name = eval.vectorizer.get_feature_names_out()
+        #     for idx in non_zero_idx:
+        #         print(f"  {features_name[idx]}")
+
+        #     print(f"prediction: {eval.predict(case)[0]}")
+
+        #     print("\n")
+        #     print(case)
+
+        # predictions = eval.predict(test_cases)
+        # for pred, label in zip(predictions, labels):
+        #     print(
+        #         f"Predicted damage taken against skulking colony elite after taking {label}:\n {pred:.2f}"
+        #     )
 
 
 if __name__ == "__main__":
